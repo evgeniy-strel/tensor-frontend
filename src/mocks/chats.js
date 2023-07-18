88c53657938d0aa603e60bec5d73a61e434b5569
evgeniy-strel <evgeniy.strel@yandex.ru>
2023-07-19 01:31:42 +0500

    Обновление верстки списка чатов с учетом групп и подгрупп

--- src/mocks/chats.js
+++ src/mocks/chats.js
@@ -4,84 +4,138 @@ export const allChats = [
     name: "Полина коленка",
     img: "/img/ls-avatar.png",
     isGroup: false,
-    lastMessage: "Привет!",
+    subChats: null,
+    lastMessage: { text: "Привет" },
   },
   {
     id: 1,
     name: "Орегона",
     img: "/img/group-avatar.png",
     isGroup: true,
-    lastMessage: "Слишком много слов чтоб влезть",
+    subChats: null,
+    lastMessage: {
+      user: {
+        img: null,
+        username: "Алексей",
+      },
+      text: "Слишком много слов чтобы влезть",
+    },
   },
   {
     id: 2,
     name: "Полина коленка",
     img: "/img/ls-avatar.png",
     isGroup: false,
-    lastMessage: "Привет!",
+    subChats: null,
+    lastMessage: { text: "Привет" },
   },
   {
     id: 3,
     name: "Полина коленка",
     img: "/img/ls-avatar.png",
     isGroup: false,
-    lastMessage: "Привет!",
+    subChats: null,
+    lastMessage: { text: "Привет" },
   },
   {
     id: 4,
     name: "Орегона",
     img: "/img/group-avatar.png",
     isGroup: true,
-    lastMessage: "Привет!",
+    subChats: ["Общий чат", "Собрание 1", "Собрание 2", "Собрание 3"],
+    lastMessage: {
+      user: {
+        img: null,
+        username: "Алексей",
+      },
+      text: "Привет!",
+    },
   },
   {
     id: 5,
     name: "Орегона",
     img: "/img/group-avatar.png",
     isGroup: true,
-    lastMessage: "Привет!",
+    subChats: null,
+    lastMessage: {
+      user: {
+        img: null,
+        username: "Алексей",
+      },
+      text: "Привет!",
+    },
   },
   {
     id: 6,
     name: "Полина коленка Коленкновна Кола",
     img: "/img/ls-avatar.png",
     isGroup: false,
-    lastMessage: "Привет!",
+    subChats: null,
+    lastMessage: { text: "Привет" },
   },
   {
     id: 7,
     name: "Орегона",
     img: "/img/group-avatar.png",
     isGroup: true,
-    lastMessage: "Привет!",
+    subChats: ["Общий чат", "Собрание 1", "Собрание 2", "Собрание 3"],
+    lastMessage: {
+      user: {
+        img: null,
+        username: "Алексей",
+      },
+      text: "Привет!",
+    },
   },
   {
     id: 8,
     name: "Орегона",
     img: "/img/group-avatar.png",
     isGroup: true,
-    lastMessage: "Привет!",
+    subChats: null,
+    lastMessage: {
+      user: {
+        img: null,
+        username: "Алексей",
+      },
+      text: "Привет!",
+    },
   },
   {
     id: 9,
     name: "Орегона",
     img: "/img/group-avatar.png",
     isGroup: true,
-    lastMessage: "Привет!",
+    subChats: null,
+    lastMessage: {
+      user: {
+        img: null,
+        username: "Алексей",
+      },
+      text: "Привет!",
+    },
   },
   {
     id: 10,
     name: "Полина коленка",
     img: "/img/ls-avatar.png",
     isGroup: false,
-    lastMessage: "Привет!",
+    subChats: null,
+    lastMessage: { text: "Привет" },
   },
   {
     id: 11,
     name: "Орегона",
     img: "/img/group-avatar.png",
     isGroup: true,
-    lastMessage: "Привет!",
+    subChats: null,
+    lastMessage: {
+      user: {
+        img: null,
+        username: "Алексей",
+      },
+      text: "Привет!",
+    },
   },
 ];
 
@@ -91,48 +145,128 @@ export const myChats = [
     name: "Орегона",
     img: "/img/group-avatar.png",
     isGroup: true,
-    lastMessage: "Слишком много слов чтоб влезть",
+    subChats: null,
+    lastMessage: {
+      user: {
+        img: null,
+        username: "Алексей",
+      },
+      text: "Слишком много слов чтобы влезть",
+    },
   },
   {
     id: 2,
     name: "Полина коленка",
     img: "/img/ls-avatar.png",
     isGroup: false,
-    lastMessage: "Привет!",
+    subChats: null,
+    lastMessage: { text: "Привет" },
   },
   {
     id: 3,
     name: "Полина коленка",
     img: "/img/ls-avatar.png",
     isGroup: false,
-    lastMessage: "Привет!",
+    subChats: null,
+    lastMessage: { text: "Привет" },
   },
   {
     id: 4,
     name: "Орегона",
     img: "/img/group-avatar.png",
     isGroup: true,
-    lastMessage: "Привет!",
+    subChats: ["Общий чат", "Собрание 1", "Собрание 2", "Собрание 3"],
+    lastMessage: {
+      user: {
+        img: null,
+        username: "Алексей",
+      },
+      text: "Привет!",
+    },
   },
   {
     id: 5,
     name: "Орегона",
     img: "/img/group-avatar.png",
     isGroup: true,
-    lastMessage: "Привет!",
+    subChats: null,
+    lastMessage: {
+      user: {
+        img: null,
+        username: "Алексей",
+      },
+      text: "Привет!",
+    },
   },
   {
     id: 6,
     name: "Полина коленка Коленкновна Кола",
     img: "/img/ls-avatar.png",
     isGroup: false,
-    lastMessage: "Привет!",
+    subChats: null,
+    lastMessage: { text: "Привет" },
   },
   {
     id: 7,
     name: "Орегона",
     img: "/img/group-avatar.png",
     isGroup: true,
-    lastMessage: "Привет!",
+    subChats: ["Общий чат", "Собрание 1", "Собрание 2", "Собрание 3"],
+
+    lastMessage: {
+      user: {
+        img: null,
+        username: "Алексей",
+      },
+      text: "Привет!",
+    },
+  },
+];
+
+export const subChats = [
+  {
+    id: 12,
+    name: "Общий чат",
+    img: "/img/ls-avatar.png",
+    isGroup: false,
+    subChats: null,
+    subChats: null,
+    lastMessage: {
+      user: {
+        img: null,
+        username: "Алексей",
+      },
+      text: "Привет!",
+    },
+  },
+  {
+    id: 13,
+    name: "Встреча 1",
+    img: "/img/ls-avatar.png",
+    isGroup: false,
+    subChats: null,
+    subChats: null,
+    lastMessage: {
+      user: {
+        img: null,
+        username: "Алексей",
+      },
+      text: "Привет!",
+    },
+  },
+  {
+    id: 14,
+    name: "Встреча 2",
+    img: "/img/ls-avatar.png",
+    isGroup: false,
+    subChats: null,
+    subChats: null,
+    lastMessage: {
+      user: {
+        img: null,
+        username: "Алексей",
+      },
+      text: "Привет!",
+    },
   },
 ];

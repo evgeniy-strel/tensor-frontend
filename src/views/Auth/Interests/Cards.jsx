99fa3805ac0fe4e742f004067f1fa01c0f9652ff
Evgeniy <82458628+evgeniy-strel@users.noreply.github.com>
2023-07-28 04:06:44 +0500

    Сокрытие кастомных тегов для чата

--- src/views/Auth/Interests/Cards.jsx
+++ src/views/Auth/Interests/Cards.jsx
@@ -1,15 +1,24 @@
 import { CardGrid, Card, Text, Counter } from "@vkontakte/vkui";
 import classes from "../auth.module.scss";
 import { useSelector, useDispatch } from "react-redux";
+import { useLocation } from "react-router";
 import { Icon28PrivacyOutline } from "@vkontakte/icons";
 
 const Cards = ({ tags, onClick }) => {
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.categories.categories);
 
+  const location = useLocation();
+  const pathname = location.pathname;
+  const hideUser = pathname?.includes("create_chat");
+
+  const categoriesFiltered = hideUser
+    ? categories.filter((category) => category?.title != "Пользовательская")
+    : categories;
+
   return (
     <CardGrid className={classes.card_container} size="s">
-      {categories.map((category) => (
+      {categoriesFiltered.map((category) => (
         <Card
           className={
             classes.card +
@@ -19,8 +28,7 @@ const Cards = ({ tags, onClick }) => {
           }
           onClick={() => onClick(category)}
           mode="outline"
-          key={category.id}
-        >
+          key={category.id}>
           {tags.filter((el) => el.category_id === category.id).length > 0 && (
             <Counter
               mode="primary"
